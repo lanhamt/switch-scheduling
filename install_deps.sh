248dd1945de87af7e2f790d65eed17319e71165d
lanhamt <tlanham@cs.stanford.edu>
2017-06-05 15:42:07 -0700

    updated install script for ubuntu

---
+++ install_deps.sh
@@ -0,0 +1,9 @@
+# Install script for Ubuntu 16.04 LTS.
+# Installs dependencies for reproducing sim switch
+# simulator runs for switch scheduler research.
+apt-get -y update
+apt -y install cowsay
+apt -y install pip
+apt -y install python-pip
+apt-get -y install python-matplotlib
+apt-get -y install build-essential
